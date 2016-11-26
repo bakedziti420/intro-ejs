f6390761f6f9728db3f257e4bd084c5d50c3a88a
ZebGirouard <timzebgir@gmail.com>
2016-11-25 17:18:37 -0700

    Creating soln branch

---
+++ solution-code/app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var ejs = require('ejs');
+var app = express();
+
+var data = {
+	title: "Class Supplies",
+	supplies: [
+		"Marker",
+		"Whiteboard",
+		"Laptop",
+		"Pen",
+		"Notebook"
+	]
+};
+
+// var html = "<h1>"+data.title+"</h1>";
+// html += "<ul>";
+// for(var i=0; i<data.supplies.length; i++) {
+//     html += "<li><a href='supplies/"+data.supplies[i]+"'>";
+//     html += data.supplies[i]+"</a></li>";
+// }
+// html += "</ul>";
+
+ejs.renderFile('./class.ejs', data, function(err,str) {
+	html = str;
+});
+
+app.get('/', function(req,res) {
+	res.send(html);
+});
+
+app.listen(3000);
